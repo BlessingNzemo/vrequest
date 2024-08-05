9364c291dbaa596dd47440ff70b7c7c5df70eb7b
salomon <salomonluveto2023@gmail.com>
2024-08-05 18:54:05 +0100

    Dashboard design

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -5,18 +5,20 @@ namespace App\Http\Controllers;
 use App\Models\Course;
 use App\Models\Demande;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Session;
 
 class HomeController extends Controller
 {
     public function index()
     {
+        /*
         if(Session::get('authUser')->hasRole('charroi')){
             $demandes =  Demande::where('is_validated',1);
             $demandes_validees = $demandes->get();
  
             $courses = Course::leftJoin('demandes','demandes.id','courses.demande_id')  ;
-            // dd($courses);
+            dd($courses);
         }else if(Session::get('authUser')->hasRole('chauffeur')){
             $user_id = Session::get('authUser')->id;
             $demandes = Demande::where('user_id',$user_id);
@@ -35,18 +37,29 @@ class HomeController extends Controller
             $courses = Course::leftJoin('demandes','demandes.id','courses.demande_id')  
                                 ->where('demandes.user_id',$user_id)
                                 ->where('chauffeur_id',$user_id);
+              
         }
-       
-        $demandes_traitees = $demandes->where('status',1)->get();
-        $demandes_en_attente =Demande::where('status',0)->get();
-       
+                                */
+        $id = Session::get('authUser')->id;
+        $demandes = Demande::where('user_id',$id)->get();
+        foreach($demandes as $demande){
+            $demande_id[] = $demande->id;
+        }
 
-        $courses_en_attente = $courses->where('demandes.status',0)->get();
-        $courses_en_cours = $courses->where('demandes.status',0) ->get();
-        $courses_terminees = $courses->where('demandes.status',1)->get();
-        //dd($demandes_validees);
+        $demandes_traitees = Demande::where('user_id',$id)->where('status',1)->get();
+        $demandes_en_attente =Demande::where('user_id',$id)->where('status',0)->get();
+        $demandes_rejetees = Demande::where('user_id',$id)->where('status',2)->get();
+        $demandes_recentes = Demande::where('user_id',$id)->latest()->take(3)->get();
         
-        return view('dashboard', compact('demandes_validees','demandes_traitees','demandes_en_attente','courses_en_attente','courses_en_cours','courses_terminees'));
+       
+        
+        $courses_en_attente = DB::table('courses')->whereIn('demande_id', $demande_id)->where('status','en_attente')->get();
+        
+       // $courses_en_cours = $courses->where('demandes.status',0) ->get();
+        //$courses_terminees = $courses->where('demandes.status',1)->get();
+        
+        
+        return view('dashboard', compact('demandes_traitees','demandes_en_attente','courses_en_attente',/*'courses_en_cours','courses_terminees',*/'demandes_rejetees','demandes','demandes_recentes'));
 
     }
 }
--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -1,4 +1,60 @@
 <div>
+    <nav class="fixed top-0 z-50 w-full bg-white border-b border-gray-200 dark:bg-gray-800 dark:border-gray-700">
+        <div class="px-3 py-3 lg:px-5 lg:pl-3">
+          <div class="flex items-center justify-between">
+            <div class="flex items-center justify-start rtl:justify-end">
+              <button data-drawer-target="logo-sidebar" data-drawer-toggle="logo-sidebar" aria-controls="logo-sidebar" type="button" class="inline-flex items-center p-2 text-sm text-gray-500 rounded-lg sm:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600">
+                  <span class="sr-only">Open sidebar</span>
+                  <svg class="w-6 h-6" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                     <path clip-rule="evenodd" fill-rule="evenodd" d="M2 4.75A.75.75 0 012.75 4h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 4.75zm0 10.5a.75.75 0 01.75-.75h7.5a.75.75 0 010 1.5h-7.5a.75.75 0 01-.75-.75zM2 10a.75.75 0 01.75-.75h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 10z"></path>
+                  </svg>
+               </button>
+              <a href="https://flowbite.com" class="flex ms-2 md:me-24">
+                <img src="{{ asset('img/orange.png') }}" class="h-8 me-3" alt="FlowBite Logo" />
+                <span class="self-center text-xl font-semibold sm:text-2xl whitespace-nowrap dark:text-white">Vrequest</span>
+              </a>
+            </div>
+            <div class="flex items-center">
+                <div class="flex items-center ms-3">
+                  <div>
+                    <button type="button" class="flex text-sm bg-white rounded-full focus:ring-4 focus:ring-gray-300 dark:focus:ring-gray-600" aria-expanded="false" data-dropdown-toggle="dropdown-user">
+                      <span class="sr-only">Open user menu</span>
+                      <img class="w-8 h-8 rounded-full" src="{{ asset('img/profil.jpeg') }}" alt="user photo">
+                    </button>
+                  </div>
+                  <div class="z-50 hidden my-4 text-base list-none bg-white divide-y divide-gray-100 rounded shadow dark:bg-gray-700 dark:divide-gray-600" id="dropdown-user">
+                    <div class="px-4 py-3" role="none">
+                      <p class="text-sm text-gray-900 dark:text-white" role="none">
+                        {{-- {{ Auth::user()->name }} --}}
+                        {{Session::get('authUser')->username}}
+                      </p>
+                      <p class="text-sm font-medium text-gray-900 truncate dark:text-gray-300" role="none">
+                        {{-- {{ Auth::user()->email }} --}}
+                        {{Session::get('authUser')->email}}
+                      </p>
+                    </div>
+                    <ul class="py-1" role="none">
+                      <li>
+                        <a href="{{ route('profile.edit') }}" class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:text-gray-300 dark:hover:bg-gray-600 dark:hover:text-white" role="menuitem">Profil</a>
+                      </li>
+                      <li>
+                            <!-- Authentication -->
+                            <form method="POST" action="{{ route('logout') }}">
+                                @csrf
+                                <x-dropdown-link :href="route('logout')"
+                                        onclick="event.preventDefault();
+                                                    this.closest('form').submit();">
+                                    {{ __('Deconnexion') }}
+                                </x-dropdown-link>
+                            </form>
+                      </li>
+                    </ul>
+                  </div>
+                </div>
+              </div>
+          </div>
+        </div>
+      </nav>
     
     
     <i class="icon">&#128197;</i>
@@ -76,84 +132,8 @@
                         <span class="flex-1 ms-3 whitespace-nowrap">Demandes</span>
                     </a>
                 </li>
-                <li>
-                    <a href="#"
-                        class="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
-                        <svg class="w-6 h-6 text-gray-800 dark:text-white" aria-hidden="true"
-                            xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
-                            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                                d="M16 7a4 4 0 1 1-8 0 4 4 0 0 1 8 0Zm-4 7a7 7 0 0 0-7 7h14a7 7 0 0 0-7-7Z" />
-                        </svg>
-                        <span class="flex-1 ms-3 whitespace-nowrap">Profil</span>
-                    </a>
-                </li>
-                <li>
-                    <a href="#"
-                        class="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
-                        <svg class="w-6 h-6 text-gray-800 dark:text-white" aria-hidden="true"
-                            xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 21">
-                            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"
-                                stroke-width="2"
-                                d="M10 3.464V1.1m0 2.365a5.338 5.338 0 0 1 5.133 5.368v1.8c0 2.386 1.867 2.982 1.867 4.175C17 15.4 17 16 16.462 16H3.538C3 16 3 15.4 3 14.807c0-1.193 1.867-1.789 1.867-4.175v-1.8A5.338 5.338 0 0 1 10 3.464ZM1.866 8.832a8.458 8.458 0 0 1 2.252-5.714m14.016 5.714a8.458 8.458 0 0 0-2.252-5.714M6.54 16a3.48 3.48 0 0 0 6.92 0H6.54Z" />
-                        </svg>
-
-                        <span class="flex-1 ms-3 whitespace-nowrap">Noctifications</span>
-                    </a>
-                </li>
-                <ul class="mt-auto">
-                    <li>
-                        <div class="flex items-center ms-3">
-                            <div>
-                                <button type="button"
-                                    class="flex text-sm bg-white rounded-full focus:ring-4 focus:ring-gray-300 dark:focus:ring-gray-600"
-                                    aria-expanded="false" data-dropdown-toggle="dropdown-user">
-                                    <span class="sr-only">Open user menu</span>
-                                    <img class="w-8 h-8 rounded-full" src="{{ asset('img/profil.jpeg') }}"
-                                        alt="user photo">
-
-                                </button>
-                            </div>
-                            <div class="z-50 hidden my-4 text-base list-none bg-white divide-y divide-gray-100 rounded shadow dark:bg-gray-700 dark:divide-gray-600"
-                                id="dropdown-user">
-                                <div class="px-4 py-3" role="none">
-                                    <p class="text-sm text-gray-900 dark:text-white" role="none">
-                                        {{-- {{ Auth::user()->name }} --}}
-                                        {{ Session::get('authUser')->username }}
-                                    </p>
-                                    <p class="text-sm font-medium text-gray-900 truncate dark:text-gray-300"
-                                        role="none">
-                                        {{-- {{ Auth::user()->email }} --}}
-                                        {{ Session::get('authUser')->email }}
-                                    </p>
-                                </div>
-                                <ul class="py-1" role="none">
-                                    <li>
-                                        <a href="{{ route('profile.edit') }}"
-                                            class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:text-gray-300 dark:hover:bg-gray-600 dark:hover:text-white"
-                                            role="menuitem">Profil</a>
-                                    </li>
-
-                                    <li>
-
-                                        <!-- Authentication -->
-                                        <form method="POST" action="{{ route('logout') }}">
-                                            @csrf
-
-                                            <x-dropdown-link :href="route('logout')"
-                                                onclick="event.preventDefault();
-                                                    this.closest('form').submit();">
-                                                {{ __('Deconnexion') }}
-                                            </x-dropdown-link>
-                                        </form>
-
-
-                                    </li>
-                                </ul>
-                            </div>
-                        </div>
-                    </li>
-
-                </ul>
+           
+                
 
 
 
