67f0d3f050dabf3eb1984d2fa40a1aed2a19533f
zgeniuscoders <zgeniuscoders@gmail.com>
2024-09-10 13:48:40 +0100

    gérer le zoom de la carte

--- resources/views/components/destinationCarte.blade.php
+++ resources/views/components/destinationCarte.blade.php
@@ -10,7 +10,7 @@
     });
 
     // Choisir la destination sur la carte
-    var mapid = L.map('mapid').setView([15.3031, -4.3322], 2);
+    var mapid = L.map('mapid').setView([-4.322693, 15.271774], 13);
     L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
         attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
     }).addTo(mapid);
