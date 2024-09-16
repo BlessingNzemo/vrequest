1304a05c3688ef200966dd2b4947d5325df68586
Baptiste Mutemba <bapmutembs@gmail.com>
2024-09-16 18:14:32 +0100

    fix: resolution d'un bugg dans action edit et delete d'une demande

--- resources/views/components/deleteDemande.blade.php
+++ resources/views/components/deleteDemande.blade.php
@@ -20,7 +20,7 @@
                     <form action="" method="post">
                         @csrf
                         @method("DELETE")
-                        <button data-modal-hide="delete-modal" type="button" class="text-white bg-red-600 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 dark:focus:ring-red-800 font-medium rounded-lg text-sm inline-flex items-center px-5 py-2.5 text-center">
+                        <button data-modal-hide="delete-modal" type="submit" class="text-white bg-red-600 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 dark:focus:ring-red-800 font-medium rounded-lg text-sm inline-flex items-center px-5 py-2.5 text-center">
                             Oui
                         </button>
                         <button data-modal-hide="delete-modal" type="button" class="py-2.5 px-5 ms-3 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-100 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700">Non</button>
@@ -32,7 +32,8 @@
     <script>
          function supprimer(event) {
             event.preventDefault()
-            const lien = event.target.getAttribute('href')
+            const a = document.querySelector("#action-delete")
+            const lien = a.getAttribute('href')
             const form=document.querySelector("#delete-modal form")
             form.setAttribute('action', lien)
         }
